125ec25a7234eb6e3dcc1e608c1f03d2c225399b
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-09-21 18:47:33 +0800

    todo

--- src/github.com/cmu440/bitcoin/server/server.go
+++ src/github.com/cmu440/bitcoin/server/server.go
@@ -25,6 +25,14 @@ func main() {
 	}
 	defer s.Close()
 
+	var availableMiner chan int = make(chan int, 10000)
+	var workingMiner map[int]int = make(map[int]int)				//key is miner connID, value is requestID
+	var requestSplitCount map[int]int = make(map[int]int)
+	var requestResult map[int]bitcoin.Message = make(map[int]bitcoin.Message)
+	var requestID = 1
+	var requestQueue chan bitcoin.Message = make(chan bitcoin.Message, 10000)	//if len(availableMiner) == 0, put request into this queue
+	var requestClient map[int]int = make(map[int]int)				//key is requestID, value is request client connID
+
 	for {
 		connID, buf, err := s.Read()
 		//handle client or miner disconnect
@@ -34,7 +42,33 @@ func main() {
 		}
 		var msg bitcoin.Message
 		json.Unmarshal(buf, &msg)
+		switch msg.Type {
+		case bitcoin.Join:
+			//miner join server
+			availableMiner <- connID
+			if len(requestQueue) > 0 {
+				request := <-requestQueue
+				minerConnID := <- availableMiner
+				s.Write(minerConnID, )
+			}
+		case bitcoin.Request:
+			//client request
 
+		case bitcoin.Result:
+			//miner return result
+
+		}
 	}
+}
 
+func splitRequest(request bitcoin.Message, availableMiner <-chan int, requestSplitCount map[int]int, requestID int, s lsp.Server) {
+	availableMinerCnt := len(availableMiner)
+	requestRange := request.Upper - request.Lower
+	perRequestRange := requestRange / availableMiner
+	for i := 0; i < availableMinerCnt; i++ {
+		perRequest := bitcoin.NewRequest(request.Data, request.Lower + uint64(i * perRequestRange), request.Lower + uint64((i+1) * perRequestRange))
+		buf, _ := json.Marshal(perRequest)
+		miner := <-availableMiner
+		s.Write(miner, buf)
+	}
 }
